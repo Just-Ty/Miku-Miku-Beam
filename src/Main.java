ec086053d7a0b0e6cbc9f20cc9b2f257c05411ba
Just-Ty <foolgrimmer@gmail.com>
2025-08-01 15:56:28 +0100

    Testing .jar

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello World");
+        if (!SystemTray.isSupported()) {
+            System.out.println("SystemTray is not supported");
 
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-
-            System.out.println("i = " + i);
         }
     }
 }
