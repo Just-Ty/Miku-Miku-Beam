529b10b4448af73e115c6403fd3d73ca7eb85caa
Just-Ty <foolgrimmer@gmail.com>
2025-08-01 16:58:24 +0100

    Added systemtray icon

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+
 import java.awt.*;
+import java.net.URL;
+import javax.swing.ImageIcon;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,5 +10,59 @@ public class Main {
             System.out.println("SystemTray is not supported");
 
         }
+
+        final PopupMenu popup = new PopupMenu();
+        final TrayIcon trayIcon =
+                new TrayIcon(createImage("images/mikuicon.png", "tray icon"));
+        final SystemTray tray = SystemTray.getSystemTray();
+
+        trayIcon.setImageAutoSize(true);
+
+        MenuItem aboutItem = new MenuItem("About");
+        CheckboxMenuItem cb1 = new CheckboxMenuItem("Set auto size");
+        CheckboxMenuItem cb2 = new CheckboxMenuItem("Set tooltip");
+        Menu displayMenu = new Menu("Display");
+        MenuItem errorItem = new MenuItem("Error");
+        MenuItem warningItem = new MenuItem("Warning");
+        MenuItem infoItem = new MenuItem("Info");
+        MenuItem noneItem = new MenuItem("None");
+        MenuItem exitItem = new MenuItem("Exit");
+
+        popup.add(aboutItem);
+        popup.addSeparator();
+        popup.add(cb1);
+        popup.add(cb2);
+        popup.addSeparator();
+        popup.add(displayMenu);
+        displayMenu.add(errorItem);
+        displayMenu.add(warningItem);
+        displayMenu.add(infoItem);
+        displayMenu.add(noneItem);
+        popup.add(exitItem);
+
+        trayIcon.setPopupMenu(popup);
+
+        try {
+            tray.add(trayIcon);
+        } catch (AWTException e) {
+            System.out.println("TrayIcon could not be added.");
+        }
+    }
+
+
+
+
+
+
+    // Helper method to load image from resources
+    protected static Image createImage(String path, String description) {
+        URL imageURL = Main.class.getResource("/" + path);
+        if (imageURL == null) {
+            System.err.println("Resource not found: " + path);
+            return null;
+        } else {
+            return (new ImageIcon(imageURL, description)).getImage();
+        }
     }
 }
+
